472e8457570cbc6870f0bef521ee9436aa35b0c8
Oychekhra <rubyhankuk@gmail.com>
2024-12-26 19:31:38 +0900

    feat: meta tags integrations on document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,26 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  console.log("DOCUMENT = PAGES ROUTER")
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+        <meta
+          name="keyword"
+          content={"nestar, nestar.uz, devex mern, mern nestjs fullstuck"}
+        />
+        <meta
+          name="description"
+          content={
+            "Buy and sell properties anywhere anytime in South Korea. | " +
+            "대한민국 언제 어디서나 부동산을 살 수 있습니다."
+          }
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
